feb1bc4fb6447fdb7b866d6e48763b38f83e64b4
SteffenLee <tnqjae@naver.com>
2023-05-19 23:39:29 +0900

    Edit code

--- Example_hello_server.c
+++ Example_hello_server.c
@@ -2,3 +2,30 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+void error_handling(char *message);
+
+int main(int argc, char *argv[]){
+    int serv_socket;
+    int clnt_socket;
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    char message[] = "Hello World!";
+
+    if(argc != 2){
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    serv_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if(serv_socket == -1)
+        error_handling("socket() error");
+    
+    memset(&serv_addr, 0 , sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+}
